d1b8eabc501e2fa64d97e9bc84c50b62c6675afd
Nam <nguyennam11022005@gmail.com>
2025-04-09 03:01:01 +1000

    Fix

--- Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/Main.cpp
+++ Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/Main.cpp
@@ -14,21 +14,52 @@ using namespace std;
 #include "Player.h"
 #include "HumanPlayer.h"
 #include "NashGame.h"
+#include "RandomPlayer.h"
+
 
 int main() {
 	int boardSize = 5;
+	int typePlayer;
+	bool checkTypePlayer = false;
 	cout <<"Input the size of board:" << endl;
     cin >> boardSize;
 	if (boardSize < 3)
 		boardSize = 3;
 
 	Board *board = new Board(boardSize);
+	while (!checkTypePlayer) {
+		cout <<"Input the type of Player:" << endl;
+		cout <<	"1. Human vs Human" << endl;
+		cout <<	"2. Random player vs Random player" << endl;
+		cout <<	"3. Random player vs Simple Mind player" << endl;
+		cout <<	"4. Monte Carlo player vs Simple Mind player" << endl;
+		cout <<	"5. Monte Carlo player vs MiniMax player" << endl;
+		cout <<	"6. MiniMax player player vs Heuristic search player" << endl;
+		cout <<	"7. MiniMax player vs human player" << endl;
+		cout <<	"8. Heuristic player vs human player" << endl;
+		cout <<	"9. Monte Carlo player vs human player" << endl;
+		cout <<	"Others: Quit" << endl;
+		cin >> typePlayer;
 
-	Player *p1 = new HumanPlayer(1, "Human (Black)");
-	Player *p2 = new HumanPlayer(-1, "Human (White)");
+		if (typePlayer != 1 && typePlayer != 2 && typePlayer != 3 && typePlayer != 4 && typePlayer != 5 &&typePlayer != 6 &&typePlayer != 7 &&typePlayer != 8 && typePlayer != 9) {
+			cout << "Invalid type of player" << endl;
+		} else {
+			checkTypePlayer = true;
+		}
+	}
+
+	Player *p1;
+	Player *p2;
+	if (typePlayer == 1) {
+		p1 = new HumanPlayer(1, "Human (Black)");
+		p2 = new HumanPlayer(-1, "Human (White)");
+	} else if (typePlayer == 2) {
+		p1 = new RandomPlayer(1, "Random (Black)");
+		p2 = new RandomPlayer(-1, "Random (White)");
+	}
 
 	NashGame game(board, p1, p2);
-	game.play();
+	game.play(typePlayer);
 
 	delete board;
 	delete p1;
--- Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/NashGame.h
+++ Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/NashGame.h
@@ -19,15 +19,23 @@ public:
 		player[1] = p2;
 	}
 
-	void play();
+	void play(int typePlayer);
 };
 
-void NashGame::play() {
+void NashGame::play(int typePlayer) {
 	int won = 0;
 	board->printBoard();
 
 	while (!won) {
+		bool isFull = board->isBoardFull();
 
+		// Terminate when board is full
+		if (isFull) {
+			cout << "The board is full, please try a new game";
+			return;
+		}
+
+		// Human Player
 		int playerType = board->getTurn();
 		int playerIndex = (playerType == player[0]->getType()) ? 0 : 1;
 		int x = -1;
@@ -49,6 +57,39 @@ void NashGame::play() {
         if(won == playerType)
         	cout << player[playerIndex]->getPlayerName() << " player wins!" << endl;
 
+		// Random Player
+		if (typePlayer == 2) {
+			if (won) {
+				return;
+			}
+			isFull = board->isBoardFull();
+			if (isFull) {
+				cout << "The board is full, please try a new game";
+				return;
+			}
+
+			// RandomPlayer
+			playerType = board->getTurn();
+			playerIndex = (playerType == player[0]->getType()) ? 0 : 1;
+			x = -1;
+			y = -1;
+
+			if (!player[playerIndex]->getMove(board, x, y)) {
+				cout << "No available move" << endl;
+				return;
+			}
+
+			cout << player[playerIndex]->getPlayerName() << " plays " << " ("
+					<< x + 1 << ", " << y + 1 << "):" << endl;
+
+			if (!board->addMove(playerType, x, y))
+				return;
+
+			board->printBoard();
+			won = board->checkWinningStatus(playerType);
+			if(won == playerType)
+				cout << player[playerIndex]->getPlayerName() << " player wins!" << endl;
+		}
 	}
 }
 
