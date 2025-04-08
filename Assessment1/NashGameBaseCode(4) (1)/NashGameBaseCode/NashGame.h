f38eefa4329db3a5ae64b12aa06b5277bf6926d3
Nam <nguyennam11022005@gmail.com>
2025-04-09 02:04:09 +1000

    added function checking is board full

--- Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/NashGame.h
+++ Assessment1/NashGameBaseCode(4) (1)/NashGameBaseCode/NashGame.h
@@ -19,15 +19,24 @@ public:
 		player[1] = p2;
 	}
 
-	void play();
+	void play(string typePlayer);
 };
 
-void NashGame::play() {
+void NashGame::play(string typePlayer) {
 	int won = 0;
+	// Task 1
 	board->printBoard();
 
 	while (!won) {
+		bool isFull = board->isBoardFull();
 
+		// Terminate when board is full
+        if (isFull) {
+        	cout << "The board is full, please try a new game";
+			return;
+        }
+
+		// HumanPlayer
 		int playerType = board->getTurn();
 		int playerIndex = (playerType == player[0]->getType()) ? 0 : 1;
 		int x = -1;
@@ -48,7 +57,38 @@ void NashGame::play() {
 		won = board->checkWinningStatus(playerType);
         if(won == playerType)
         	cout << player[playerIndex]->getPlayerName() << " player wins!" << endl;
+		if (typePlayer == "Random") {
+			if (won) {
+				return;
+			}
+			isFull = board->isBoardFull();
+			if (isFull) {
+				cout << "The board is full, please try a new game";
+				return;
+			}
 
+			// RandomPlayer
+			playerType = board->getTurn();
+			playerIndex = (playerType == player[0]->getType()) ? 0 : 1;
+			x = -1;
+			y = -1;
+
+			if (!player[playerIndex]->getMove(board, x, y)) {
+				cout << "No available move" << endl;
+				return;
+			}
+
+			cout << player[playerIndex]->getPlayerName() << " plays " << " ("
+					<< x + 1 << ", " << y + 1 << "):" << endl;
+
+			if (!board->addMove(playerType, x, y))
+				return;
+
+			board->printBoard();
+			won = board->checkWinningStatus(playerType);
+			if(won == playerType)
+				cout << player[playerIndex]->getPlayerName() << " player wins!" << endl;
+		}
 	}
 }
 
